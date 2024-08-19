932efbaf5c25fbe6e62775b8d3c797c195e2ae7e
abubakarseddek1999 <abubakarseddek1999@gmail.com>
2024-08-19 11:21:33 +0600

    made footer sectio

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Share/Navbar/Navbar";
+import Footer from "../Share/Navbar/Footer";
 
 const Main = () => {
     return (
         <div className="bg-[#EEEEEE]">
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
 
         </div>
     );
