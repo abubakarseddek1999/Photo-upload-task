f3548e9e22d0fd1bae1ce93a7814343942f2b130
abubakarseddek1999 <abubakarseddek1999@gmail.com>
2024-08-19 12:48:21 +0600

    add date on footer

--- src/Share/Navbar/Footer.jsx
+++ src/Share/Navbar/Footer.jsx
@@ -1,5 +1,7 @@
 // import moment from "moment";
 
+import moment from "moment";
+
 const Footer = () => {
     return (
         <div className="bg-[#2A7CC7] ">
@@ -33,11 +35,16 @@ const Footer = () => {
                         <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current cursor-pointer"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a>
                         <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current cursor-pointer"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
                     </div>
+                    <aside>
+                    <p className="text-xl text-white text-center font-bold">{moment().format(" MMMM D, YYYY ")}</p>
+                    </aside>
                 </nav>
             </footer>
             <footer className="flex justify-center items-center p-5 text-white">
                 @2024 Copyright:SchoolManagement.com
+                
             </footer>
+            
 
 
 
