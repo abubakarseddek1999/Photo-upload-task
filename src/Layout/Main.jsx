573a01566bbe57a503f67d0e8f9926764446de93
abubakarseddek1999 <abubakarseddek1999@gmail.com>
2024-08-19 11:09:31 +0600

    made a navbar

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../Share/Navbar/Navbar";
 
 const Main = () => {
     return (
         <div className="bg-[#EEEEEE]">
-        
+            <Navbar></Navbar>
             <Outlet></Outlet>
-            
+
         </div>
     );
 };
